492176750e8c333ea63c6d0e0747aefc9095c500
Artur Hapantsou <trayhartred@gmail.com>
2026-03-19 00:14:18 -0700

    Update Ember to v1.1.0.2

--- Casks/ember.rb
+++ Casks/ember.rb
@@ -1,15 +1,14 @@
 cask "ember" do
-  version "1.0.0"
-  sha256 "c52bd05cbe38e04152004463157a0820049b9da764e98e85aefb9ea13a5a1c0a"
+  version "1.1.0.2"
+  sha256 "0ecb2d14efd4bf29b99cbac8c653eee4db9545fddee8631829000f78d74a81aa"
 
   url "https://github.com/arcimun/ember/releases/download/v#{version}/Ember-#{version}.dmg"
   name "Ember"
   desc "Voice-to-text for macOS. Groq Whisper STT with LLM grammar correction."
   homepage "https://github.com/arcimun/ember"
 
-  depends_on formula: "sox"
-
   app "Ember.app"
+  auto_updates true
 
   zap trash: [
     "~/.config/ember",
